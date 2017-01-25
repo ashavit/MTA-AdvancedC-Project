0af270cfccdd3d1cc10d088a1df4e37d02a867fd
Amir Shavit <amir.shavit@autodesk.com>
2017-01-25 23:59:37 +0200

    Move tests to test class

---
+++ TestClass.h
@@ -0,0 +1,15 @@
+#ifndef __TEST_CLASS_H__
+#define __TEST_CLASS_H__
+
+#include <stdio.h>
+
+#include "PositionList.h"
+#include "Commons.h"
+#include "Board.h"
+#include "PositionArray.h"
+#include "pathTree.h"
+#include "PathPrices.h"
+
+void runTests();
+
+#endif /* TestClass_h */
